eeb16705d5b457d961643f574a3e486d2e8c76ae
Salman Padarwqala <salmangadyali123@gmail.com>
2023-09-23 17:31:35 +0530

    Navbar and Footer is completed

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../layout/Navbar";
+import Footer from "../layout/Footer";
 
 const index = () => {
   return (
     <>
       <Navbar />
+      <Footer />
     </>
   );
 };
